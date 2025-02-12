0ceb09a9ce0297bc36e385509dfe6473a9993b1e
Mochamad Idris <idrez.mochamad@gmail.com>
2025-02-12 18:09:51 +0000

    fix wp-content

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ tar -zxvf latest.tar.gz
 rm latest.tar.gz
 echo "Settingup Server..."
 rm wordpress/wp-config.php
-cp wp-config.php wordpress/wp-config.php
+# cp wp-config.php wordpress/wp-config.php
 export $(grep -v '^#' .env | xargs) && envsubst < httpd_config.conf.template > conf/httpd_config.conf
-
+echo "Finish..."
