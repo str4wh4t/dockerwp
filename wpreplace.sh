2f420f34758dc50f3b6e6e9064bc2931807cb898
Mochamad Idris <idrez.mochamad@gmail.com>
2025-02-13 14:26:36 +0000

    fix wpreplace.sh

--- wpreplace.sh
+++ wpreplace.sh
@@ -1,14 +1,14 @@
 # Download and Install WordPress
 echo "Backingup wp-content..."
-mv wordpress/wp-content wordpress/wp-content.bak
+mv wordpress/wp-content wordpress/wp-content-bak
 echo "Downloading WP..."
 cd .
 wget https://wordpress.org/latest.tar.gz
 tar -zxf latest.tar.gz
 rm latest.tar.gz
 echo "Returning wp-content..."
-rm wordpress/wp-content
-mv wordpress/wp-content.bak wordpress/wp-content
+rm -rf wordpress/wp-content
+mv wordpress/wp-content-bak wordpress/wp-content
 rm wordpress/wp-config.php
 cp wp-config.php wordpress/wp-config.php
 echo "Finish."
